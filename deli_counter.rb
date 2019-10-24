5ce377290c310785c850cad30337bd1baf3938d6
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-24 18:02:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,9 @@ def line(deli)
   end
 end
 
-def take_a_number
-
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
 def now_serving
